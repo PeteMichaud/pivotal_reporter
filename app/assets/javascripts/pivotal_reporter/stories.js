9681bce66aea73395679e48e3d1a2e6590d7fdaa
Pete Michaud <michaudp@gmail.com>
2012-11-11 20:17:06 -0600

    added javascript to enable passing the rejection message

--- app/assets/javascripts/pivotal_reporter/stories.js
+++ app/assets/javascripts/pivotal_reporter/stories.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+    $('.reject-btn').click(function(e){
+        var msg = '';
+        while (msg == '' || msg == null)
+        {
+            msg = prompt("Reason for Rejection:");
+            if (msg == null)
+            {
+                alert("You must enter a reason for your rejection.");
+            }
+        }
+
+        window.location = $(this).data('reject-path') + "?reject_message=" + encodeURI(msg);
+
+        return false;
+    });
+});
