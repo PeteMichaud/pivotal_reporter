8cf12e7278b6453c1afe1902dd31d2179dd74749
Pete Michaud <michaudp@gmail.com>
2012-11-11 20:17:27 -0600

    added accept and reject story routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 PivotalReporter::Engine.routes.draw do
 
-  resources :stories
+  resources :stories do
+    member do
+      get 'accept' => 'stories#accept', to: :accept_story
+      get 'reject' => 'stories#reject', to: :reject_story
+    end
+  end
 
   root :to => 'stories#new'
 
